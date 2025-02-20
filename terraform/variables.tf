f49f55e9cb84f664ca2e4ebe3301404afb6f5ec3
VitaliWin <windows@my.by>
2025-02-20 17:50:42 +0300

    variables tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,17 @@
 variable "app_version" {
   description = "Application version tag"
   type        = string
-  default     = "latest"
-}
+  }
 variable "project_id" {
   description = "project GCP"
   type        = string
-  default     = "final-448512"
+  
 }
 
 variable "region" {
   description = "Region for resources"
   type        = string
-  default     = "europe-west1"
+  
 }
 variable "zone" {
   description = "Zone for resources"
